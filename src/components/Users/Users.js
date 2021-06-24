3dec2b942195fe60eee07221db2be95b6ac7ce45
OrestSochinskiy <sochinskiyorest@gmail.com>
2021-06-24 18:02:50 +0300

    practic1_addTask

---
+++ src/components/Users/Users.js
@@ -0,0 +1,24 @@
+import {useEffect, useState} from "react";
+import User from "../PostUser/PostUser";
+
+export default function Users(props) {
+
+    let [users,setUsers] = useState([]);
+    useEffect(() =>
+    {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(usersfromServer => {
+
+                setUsers(usersfromServer)
+            })
+
+    },[])
+    return(
+        <div>
+            {
+                users.map(user => <User item={user}/>)
+            }
+        </div>
+    );
+}
