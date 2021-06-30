d0f8116e32e7281825a62f1ff3f55bc7abed54a9
OrestSochinskiy <sochinskiyorest@gmail.com>
2021-06-30 16:00:02 +0300

    dz_3(Reducer)

---
+++ src/components/posts/Posts.js
@@ -0,0 +1,25 @@
+import {useEffect, useState} from "react";
+import Post from "../post/Post";
+
+export default function Posts(props) {
+
+    let [posts,setPosts] = useState([]);
+    useEffect(() =>
+    {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(value => value.json())
+            .then(usersfromServer => {
+
+                setPosts(usersfromServer)
+
+            })
+
+    },[])
+    return(
+        <div>
+            {
+                posts.map(post => <Post item={post}/>)
+            }
+        </div>
+    );
+}
---
+++ src/components/users/Users.js
@@ -0,0 +1,27 @@
+
+import {useEffect, useState} from "react";
+import User from "../user/User";
+
+
+export default function Users(props) {
+
+    let [users,setUsers] = useState([]);
+    useEffect(() =>
+    {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(usersfromServer => {
+
+                setUsers(usersfromServer)
+
+            })
+
+    },[])
+    return(
+        <div>
+            {
+                users.map(user => <User item={user}/>)
+            }
+        </div>
+    );
+}
